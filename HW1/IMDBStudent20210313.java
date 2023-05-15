7412e625fd80038fd7985c64244c6f37a27a2ef3
ymkdev <eveminful@gmail.com>
2023-05-15 20:16:29 +0900

    hw1-1

---
+++ HW1/IMDBStudent20210313.java
@@ -0,0 +1,74 @@
+import java.io.IOException;
+import java.util.*;
+
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.*;
+import org.apache.hadoop.mapreduce.lib.output.*;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+public class IMDBStudent20210313
+{
+        public static class IMDBStudent20210313Mapper extends Mapper<Object, Text, Text, IntWritable>
+        {
+                private final static IntWritable one = new IntWritable(1);
+                private Text word = new Text();
+
+                public void map(Object key, Text value, Context context) throws IOException, InterruptedException
+                {
+                        StringTokenizer itr = new StringTokenizer(value.toString(),"::");
+                	String s1="";      
+			while(itr.hasMoreTokens()){
+				s1 = itr.nextToken();
+
+			}
+			StringTokenizer itr2 = new StringTokenizer(s1,"|");
+		       	while (itr2.hasMoreTokens())
+                        {
+                                word.set(itr2.nextToken());
+                                context.write(word, one);
+                        }
+                }
+        }
+      
+	public static class IMDBStudent20210313Reducer extends Reducer<Text,IntWritable,Text,IntWritable>
+        {
+                private IntWritable result = new IntWritable();
+
+                public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException
+                {
+                        int sum = 0;
+                        for (IntWritable val : values)
+                        {
+                                sum += val.get();
+                        }
+                        result.set(sum);
+                        context.write(key, result);
+                }
+        }
+
+        public static void main(String[] args) throws Exception
+        {
+                Configuration conf = new Configuration();
+                String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+                if (otherArgs.length != 2)
+                {
+                        System.err.println("Usage: wordcount <in> <out>");
+                        System.exit(2);
+                }
+                Job job = new Job(conf, "IMDBStudent20210313");
+                job.setJarByClass(IMDBStudent20210313.class);
+                job.setMapperClass(IMDBStudent20210313Mapper.class);
+                job.setCombinerClass(IMDBStudent20210313Reducer.class);
+                job.setReducerClass(IMDBStudent20210313Reducer.class);
+                job.setOutputKeyClass(Text.class);
+                job.setOutputValueClass(IntWritable.class);
+                FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+                FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+                FileSystem.get(job.getConfiguration()).delete( new Path(otherArgs[1]), true);
+                System.exit(job.waitForCompletion(true) ? 0 : 1);
+        }
+}                         

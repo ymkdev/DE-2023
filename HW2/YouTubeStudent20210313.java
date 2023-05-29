df548d6ab141c27afce871cba56db3f6ed08f3b2
ymkdev <97513263+ymkdev@users.noreply.github.com>
2023-05-29 20:09:42 +0900

    Update YouTubeStudent20210313.java
    ss

--- HW2/YouTubeStudent20210313.java
+++ HW2/YouTubeStudent20210313.java
@@ -10,19 +10,18 @@ import org.apache.hadoop.mapreduce.lib.input.*;
 import org.apache.hadoop.mapreduce.lib.output.*;
 import org.apache.hadoop.util.GenericOptionsParser;
 
-public class YouTubeStudent20210313 {
- 	public String category;
- 	public double rating;
- 	public YouTubeStudent20210313(String category, double rating) {
+class YouTube{
+	public String category;
+	public double rating;
+	public YouTube(String category, double rating) {
 		this.category = category;
    		this.rating = rating;
   	}
-  	public String getString()
-  	{
-    		return category +" " +rating;
-  	}
-  	public static class YouTubeStudent20210313Comparator implements Comparator<YouTubeStudent20210313> {
-    		public int compare(YouTubeStudent20210313 x, YouTubeStudent20210313 y) {
+}
+public class YouTubeStudent20210313 {
+ 	
+  	public static class YouTubeStudent20210313Comparator implements Comparator<YouTube> {
+    		public int compare(YouTube x, YouTube y) {
       			if ( x.rating > y.rating ) return 1;
       			if ( x.rating < y.rating ) return -1;
       			return 0;
@@ -30,33 +29,34 @@ public class YouTubeStudent20210313 {
   	}
   
  	 public static void insertYouTubeStudent20210313(PriorityQueue q, String category, double rating, int topK) {
-	    	YouTubeStudent20210313 YouTubeStudent20210313_head = (YouTubeStudent20210313) q.peek();
+	    	YouTube YouTubeStudent20210313_head = (YouTube) q.peek();
     		if ( q.size() < topK || YouTubeStudent20210313_head.rating < rating )
     		{
-      			YouTubeStudent20210313 youtubestudent20210313 = new YouTubeStudent20210313(category, rating);
+      			YouTube youtubestudent20210313 = new YouTube(category, rating);
       			q.add( youtubestudent20210313 );
       			if( q.size() > topK ) q.remove();
     		}
   	}
   
- 	 public static class TopKMapper extends Mapper<Object, Text, Text, NullWritable> {
-   		 private PriorityQueue<YouTubeStudent20210313> queue ;
-  		 private Comparator<YouTubeStudent20210313> comp = new YouTubeStudent20210313Comparator();
-    		 private int topK;
+ 	 public static class YouTubeStudent20210313Mapper extends Mapper<Object, Text, Text, NullWritable> {
+   	//	 private PriorityQueue<YouTubeStudent20210313> queue ;
+  	//	 private Comparator<YouTubeStudent20210313> comp = new YouTubeStudent20210313Comparator();
+    	//	 private int topK;
     		 public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
       			StringTokenizer itr = new StringTokenizer(value.toString(), "|");
      			itr.nextToken();
       			itr.nextToken();
       			itr.nextToken();
-      			itr.nextToken();
+      			//itr.nextToken();
      			String category = itr.nextToken();
       			itr.nextToken();
       			itr.nextToken();
       			double rating = Double.parseDouble(itr.nextToken());
-      			insertYouTubeStudent20210313(queue, category, rating, topK);
+      			//insertYouTubeStudent20210313(queue, category, rating, topK);
+			context.write(new Text(category), new DoubleWritable(rating));
   		}
   
-  		protected void setup(Context context) throws IOException, InterruptedException {
+  		/*protected void setup(Context context) throws IOException, InterruptedException {
     			Configuration conf = context.getConfiguration();
     			topK = conf.getInt("topK", -1);
     			queue = new PriorityQueue<YouTubeStudent20210313>( topK , comp);
@@ -67,35 +67,36 @@ public class YouTubeStudent20210313 {
       			YouTubeStudent20210313 youtubestudent20210313 = (YouTubeStudent20210313) queue.remove();
       			context.write( new Text( youtubestudent20210313.getString() ), NullWritable.get() );
     			}
-  		}
+  		}*/
 	}
-	public static class TopKReducer extends Reducer<Text,NullWritable,Text,NullWritable> {
-    		private PriorityQueue<YouTubeStudent20210313> queue ;
-    		private Comparator<YouTubeStudent20210313> comp = new YouTubeStudent20210313Comparator();
+	public static class YouTubeStudent20210313Reducer extends Reducer<Text,DoubleWritable,Text,DoubleWritable> {
+    		private PriorityQueue<YouTube> queue ;
+    		private Comparator<YouTube> comp = new YouTubeStudent20210313Comparator();
     		private int topK;
-    		public void reduce(Text key, Iterable<NullWritable> values, Context context) throws IOException, InterruptedException {
-      			StringTokenizer itr = new StringTokenizer(key.toString(), " ");
-      //itr.nextToken();
-      //itr.nextToken();
-      //itr.nextToken();
-      //itr.nextToken();
-      			String category = itr.nextToken();
-      //itr.nextToken();
-      //itr.nextToken();
-      			double rating = Double.parseDouble(itr.nextToken());
-      			insertYouTubeStudent20210313(queue, category, rating, topK);
+    		public void reduce(Text key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException {
+      			double sum =0;
+			double average=0;
+			int count =0;
+			
+			for(DoubleWritable val : values){
+				sum += val.get();
+				count++;
+			}
+			average = sum/count;
+		
+      			insertYouTubeStudent20210313(queue, key.toString(), average, topK);
   		}
   
   		protected void setup(Context context) throws IOException, InterruptedException {
     			Configuration conf = context.getConfiguration();
     			topK = conf.getInt("topK", -1);
-    			queue = new PriorityQueue<YouTubeStudent20210313>( topK , comp);
+    			queue = new PriorityQueue<YouTube>( topK , comp);
   		}
     
   		protected void cleanup(Context context) throws IOException, InterruptedException {
     			while( queue.size() != 0 ) {
-      			YouTubeStudent20210313 youtubestudent20210313 = (YouTubeStudent20210313) queue.remove();
-      			context.write( new Text( youtubestudent20210313.getString() ), NullWritable.get() );
+      			YouTube youtubestudent = (YouTube) queue.remove();
+      			context.write( new Text( youtubestudent.category ), new DoubleWritable(youtubestudent.rating) );
     			}
   		}
 	}
@@ -108,11 +109,11 @@ public class YouTubeStudent20210313 {
     		}
     		conf.setInt("topK", topK);
     		Job job = new Job(conf, "YouTubeStudent20210313");
-    		job.setJarByClass(TopK.class);
-    		job.setMapperClass(TopKMapper.class);
-    		job.setReducerClass(TopKReducer.class);
+    		job.setJarByClass(YouTubeStudent20210313.class);
+    		job.setMapperClass(YouTubeStudent20210313Mapper.class);
+    		job.setReducerClass(YouTubeStudent20210313Reducer.class);
     		job.setOutputKeyClass(Text.class);
-    		job.setOutputValueClass(NullWritable.class);
+    		job.setOutputValueClass(DoubleWritable.class);
     		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
     		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
     		FileSystem.get(job.getConfiguration()).delete( new Path(otherArgs[1]), true);

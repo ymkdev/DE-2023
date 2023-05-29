1782d8f527e907fd3a7ba4a26dbf9e8b1bb28c57
ymkdev <97513263+ymkdev@users.noreply.github.com>
2023-05-29 20:37:17 +0900

    Update IMDBStudent20210313.java
    g

--- HW2/IMDBStudent20210313.java
+++ HW2/IMDBStudent20210313.java
@@ -11,8 +11,36 @@ import org.apache.hadoop.mapreduce.lib.input.*;
 import org.apache.hadoop.mapreduce.lib.output.*;
 import org.apache.hadoop.util.GenericOptionsParser;
 
+class IMDB{
+	public String description
+	public double avg_rating;
+	public YouTube(String description, double avg_rating) {
+		this.description = description;
+   		this.avg_rating = avg_rating;
+  	}
+}
 public class IMDBStudent20210313
 {
+	public static class IMDBStudent20210313Comparator implements Comparator<IMDB>
+        {
+                public int compare(IMDB x, IMDB y) {
+                        if ( x.avg_rating > y.avg_rating ) return 1;
+                        if ( x.avg_rating < y.avg_rating ) return -1;
+                        return 0;
+                }
+        }
+	
+	public static void insertEmp(PriorityQueue q, String description, double avg_rating, int topK)
+        {
+                IMDB IMDBStudent20210313_head = (IMDB) q.peek();
+                if ( q.size() < topK || IMDBStudent20210313_head.avg_rating < avg_rating )
+                {
+                        IMDB imdbstudent20210313 = new IMDB(description, avg_rating);
+                        q.add( imdbstudent20210313 );
+                        if( q.size() > topK ) q.remove();
+                }
+        }
+	
         public static class IMDBStudent20210313Mapper extends Mapper<Object, Text, Text, Text>
         {
                 boolean fileA = true;
@@ -43,45 +71,12 @@ public class IMDBStudent20210313
                         if ( filename.indexOf( "relation_a" ) != -1 ) fileA = true;
                                 else fileA = false;
                 }
-        }
-        
+	}
 
-	public static class Emp
+	public static class IMDBStudent20210313Reducer extends Reducer<Text,Text,Text,DoubleWritable>
         {
-                public String description;
-		public double avg_rating;
-                public Emp(String description, double avg_rating) {
-                		this.description = description;
-				this.avg_rating = avg_rating;
-		}
-                public String getString()
-                {
-                        return description+ " "+avg_rating;
-                }
-        }
-        public static class EmpComparator implements Comparator<Emp>
-        {
-                public int compare(Emp x, Emp y) {
-                        if ( x.avg_rating > y.avg_rating ) return 1;
-                        if ( x.avg_rating < y.avg_rating ) return -1;
-                        return 0;
-                }
-        }
-        public static void insertEmp(PriorityQueue q, String description, double avg_rating, int topK)
-        {
-                Emp emp_head = (Emp) q.peek();
-                if ( q.size() < topK || emp_head.avg_rating < avg_rating )
-                {
-                        Emp emp = new Emp(description, avg_rating);
-                        q.add( emp );
-                        if( q.size() > topK ) q.remove();
-                }
-        }
-
-	public static class IMDBStudent20210313Reducer extends Reducer<Text,Text,Text,NullWritable>
-        {
-                private PriorityQueue<Emp> queue ;
-                private Comparator<Emp> comp = new EmpComparator();
+                private PriorityQueue<IMDB> queue ;
+                private Comparator<IMDB> comp = new IMDBStudent20210313Comparator();
                 private int topK;
 	       	public void reduce(Text key, Iterable<Text> values, Context context) throws IOException,
 
@@ -134,18 +129,18 @@ public class IMDBStudent20210313
 		//	reduce_key.set(description);
 		//	reduce_result.set((double)avg_rating/count);
 		//	context.write(rudece_key, reduce_result);
-			avg_rating = (double)avg_rating/count;
-			insertEmp(queue, description, avg_rating, topK);
+			double avg_rating2 = (double)avg_rating/count;
+			insertIMDBStudent20210313(queue, description, avg_rating2, topK);
                 }
 		protected void setup(Context context) throws IOException, InterruptedException {
                         Configuration conf = context.getConfiguration();
                         topK = conf.getInt("topK", -1);
-                        queue = new PriorityQueue<Emp>( topK , comp);
+                        queue = new PriorityQueue<IMDB>( topK , comp);
                 }
                 protected void cleanup(Context context) throws IOException, InterruptedException {
                         while( queue.size() != 0 ) {
-                                Emp emp = (Emp) queue.remove();
-                                context.write( new Text( emp.getString() ), NullWritable.get() );
+                                IMDB imdbstudent = (IMDB) queue.remove();
+                                context.write( new Text( imdbstudent.description ), new DoubleWritable(imdbstudent.avg_rating)); );
                         }
                 }
 
@@ -156,13 +151,14 @@ public class IMDBStudent20210313
         {
                 Configuration conf = new Configuration();
                 String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
-                int topK = 2;
-                if (otherArgs.length != 2)
+              // int topK = 2;
+                if (otherArgs.length != 3)
                 {
                         System.err.println("Usage: ReduceSideJoin <in> <out>");
                         System.exit(2);
                 }
-                conf.setInt("topK", topK);
+               // conf.setInt("topK", topK);
+		conf.setInt("topK", Integer.valueOf(otherArgs[2]));
 
                 Job job = new Job(conf, "IMDBStudent20210313");
                 job.setJarByClass(IMDBStudent20210313.class);
@@ -170,7 +166,7 @@ public class IMDBStudent20210313
                 job.setReducerClass(IMDBStudent20210313Reducer.class);
 		job.setNumReduceTasks(1);
                 job.setOutputKeyClass(Text.class);
-                job.setOutputValueClass(NullWritable.class);
+                job.setOutputValueClass(DoubleWritable.class);
                 FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
                 FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
                 FileSystem.get(job.getConfiguration()).delete( new Path(otherArgs[1]), true);

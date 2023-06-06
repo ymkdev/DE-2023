8ae1f5634fe754770f29f182b5ca506dde80a934
ymkdev <97513263+ymkdev@users.noreply.github.com>
2023-06-06 21:15:12 +0900

    Update UBERStudent20210313.java
    ㅇㅇ

--- HW3/UBERStudent20210313.java
+++ HW3/UBERStudent20210313.java
@@ -24,14 +24,16 @@ public final class UBERStudent20210313 implements Serializable {
     //JavaRDD<String> lines = spark.read().textFile(args[0]).javaRDD(); //파일 읽어오기 
     
     JavaRDD<String> products = spark.read().textFile(args[0]).javaRDD();
-    PairFunction<String, Text, Text> pfA = new PairFunction<String, Text, Text>() {
-      public Tuple2<Text, Text> call(String s) {
+    PairFunction<String, String, String> pfA = new PairFunction<String, String, String>() {
+      public Tuple2<String, String> call(String s) {
       // String을 delimiter로 자르기
       // join key를 key로 하고, Product 객체를 value로 하는 Tuple2 반환
         
         StringTokenizer itr = new StringTokenizer(s, ",");
-        Text outputKey = new Text();
-        Text outputValue = new Text();
+        //Text outputKey = new Text();
+        //Text outputValue = new Text();
+	String outputKey="";
+	String outputValue="";
         String joinKey = "";
         String o_value = "";
         String dateS = "";
@@ -65,11 +67,13 @@ public final class UBERStudent20210313 implements Serializable {
         }else if (dayOfWeek.getValue() == 7) {
             answer = "SUN";
         }
-        joinKey = joinKey+","+answer;
-	outputKey.set( joinKey );
+        //joinKey = joinKey+","+answer;
+	//outputKey.set( joinKey );
+	outputKey = joinKey+","+answer;   
         vehicles = itr.nextToken();
         trips = itr.nextToken();
-        outputValue.set(trips+","+vehicles );
+        //outputValue.set(trips+","+vehicles );
+	outputValue = trips+","+vehicles
         //context.write( outputKey, outputValue );
 
         return new Tuple2(outputKey, outputValue);

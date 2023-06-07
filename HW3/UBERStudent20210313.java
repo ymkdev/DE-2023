6977007257ab786a1d8c4fed54e668e929e2ff59
ymkdev <97513263+ymkdev@users.noreply.github.com>
2023-06-07 15:42:44 +0900

    Update UBERStudent20210313.java
    dd

--- HW3/UBERStudent20210313.java
+++ HW3/UBERStudent20210313.java
@@ -25,14 +25,14 @@ public final class UBERStudent20210313 implements Serializable {
     //JavaRDD<String> lines = spark.read().textFile(args[0]).javaRDD(); //파일 읽어오기 
     
     JavaRDD<String> products = spark.read().textFile(args[0]).javaRDD();
-    PairFunction<String, String, String> pfA = new PairFunction<String, String, String>() {
-      public Tuple2<String, String> call(String s) {
+    PairFunction<String, Text, Text> pfA = new PairFunction<String, Text, Text>() {
+      public Tuple2<Text, Text> call(String s) {
       // String을 delimiter로 자르기
       // join key를 key로 하고, Product 객체를 value로 하는 Tuple2 반환
         
         StringTokenizer itr = new StringTokenizer(s, ",");
-        //Text outputKey = new Text();
-        //Text outputValue = new Text();
+        Text outputKey = new Text();
+        Text outputValue = new Text();
 	String outputKey="";
 	String outputValue="";
         String joinKey = "";
@@ -68,36 +68,36 @@ public final class UBERStudent20210313 implements Serializable {
         }else if (dayOfWeek.getValue() == 7) {
             answer = "SUN";
         }
-        //joinKey = joinKey+","+answer;
-	//outputKey.set( joinKey );
-	outputKey = joinKey+","+answer;   
+        joinKey = joinKey+","+answer;
+	outputKey.set( joinKey );
+	//outputKey = joinKey+","+answer;   
         vehicles = itr.nextToken();
         trips = itr.nextToken();
-        //outputValue.set(trips+","+vehicles );
-	outputValue = trips+","+vehicles
+        outputValue.set(trips+","+vehicles );
+	//outputValue = trips+","+vehicles
         //context.write( outputKey, outputValue );
 
         return new Tuple2<>(outputKey, outputValue);
         
       }
     };
-    JavaPairRDD<String, String> pTuples = products.mapToPair(pfA);
+    JavaPairRDD<Text, Text> pTuples = products.mapToPair(pfA);
 
-    Function2<String, String, String> f2 = new Function2<String, String, String>() {
-      public String call(String x, String y) {
+    Function2<Text, Text, Text> f2 = new Function2<Text, Text, Text>() {
+      public String call(Text x, Text y) {
         
         
         //Text reduce_key = new Text();
-        //Text reduce_result = new Text();
+        Text reduce_result = new Text();
         int sum=0;
         int sum2=0;
         String result="";
                         
-        StringTokenizer itr5 = new StringTokenizer(x, ",");
+        StringTokenizer itr5 = new StringTokenizer(x.toString(), ",");
         int n1 =Integer.parseInt(itr5.nextToken());
         int n2 =Integer.parseInt(itr5.nextToken());
         
-        StringTokenizer itr6 = new StringTokenizer(y, ",");
+        StringTokenizer itr6 = new StringTokenizer(y.toString(), ",");
         int k1 =Integer.parseInt(itr6.nextToken());
         int k2 =Integer.parseInt(itr6.nextToken());
         
@@ -105,14 +105,14 @@ public final class UBERStudent20210313 implements Serializable {
         sum2 =n2+k2;
                  
         result= sum+","+sum2;
-        //reduce_result.set(result);
+        reduce_result.set(result);
         //context.write(key, reduce_result);
         
         
         return result;
       }
     };
-    JavaPairRDD<String, String> counts = pTuples.reduceByKey(f2);
+    JavaPairRDD<Text, Text> counts = pTuples.reduceByKey(f2);
     counts.saveAsTextFile(args[1]);
     spark.stop();
   }
